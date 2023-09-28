5c6290527ac3f9fd1ec4fb8ebddd38a799e447bf
eduards <eduards@abishevs.dev>
2023-09-28 14:08:21 +0200

    feat: structured classes for simulation and testing-in progress

--- main.py
+++ main.py
@@ -1,13 +1,11 @@
+from random import shuffle
 from poker_calc.deck.deck import Deck as Dk
 from poker_calc.utils.ranker import evaluate_hand
 from phevaluator.card import Card
 import numpy as np
 
-class Deck:
-    def __init__(self) -> None:
-        self.cards = np.array([card for card in range(52)])
 """
-TODO: 
+TODO: Overview
     1) Write diffrent shuffling algorithms enhiriting the base class 
         1.1) Algorithm: Fisher-yates (as base test)
         1.2) Riffle-shuffle
@@ -22,6 +20,89 @@ TODO:
         3.3) ApEn
     4) Analys of charts and diagrams (self done after)
 """
+"""
+TODO:
+    29.09:
+        1) Make an reusable program for running shuffles and saving them
+        2) Start building reusable test suite program, to do pattern matching, poker Test and or ApEn
+            2.1) Look into how to save pictures automaticly with matplotlib, without actualy ploting it.
+            2.2) If that works, all of the tests can be done within one program run.
+        3) ... 
+
+"""
+
+class Deck:
+    def __init__(self) -> None:
+        self.cards = np.array([card for card in range(52)])
+
+    def np_shuffle(self):
+        np.random.shuffle(self.cards) 
+
+    def fisher_yates(self):
+        pass
+        # print("FIsher Yates")
+
+    def riffle_shuffle(self):
+        pass
+
+class Simulation:
+    """Defines simulation parameters.
+    In a loop does shuffles and swaps them to pre genereated zerod 2d np.array.
+    Saves the simulation data as an .npy file with with filename that gives context of which shuffle it is
+    and how many times it shuffled the deck.
+
+
+    """
+    def __init__(self) -> None:
+        self.num_runs : int = 10
+        self.num_shuffles : int = 1 
+        self.shuffle_name = ""
+        self.raw_data = np.zeros((self.num_runs, 52), dtype='int8')  # int8 can store -+127, Do upcasting if numbers could exceed
+
+    def save(self):
+        """Saves the 2d np.array to external file
+        """
+        raw_data_file = f"{self.shuffle_name}-{self.num_shuffles}.npy" # algorith-1 means that that it shuffled 1 time
+        np.save(raw_data_file, self.raw_data)
+
+    def run(self, shuffling_algorithm_name:str):
+        """
+        Runs the simulation using predifined parameters
+        """
+        self.shuffle_name = shuffling_algorithm_name
+        for i in range(self.num_runs):
+            deck = Deck() #  
+            shuffle = getattr(deck, shuffling_algorithm_name) # gets the method of the shuffling algorithm
+            for _ in range(self.num_shuffles):
+                shuffle() # runs deck.chosen_algorithm
+
+            # swaps shuffled array with an pre existing 2d zeroed array container
+            self.raw_data[i, :] = deck.cards
+
+class Test:
+    """An base class of tests, mby if it has merit
+    """
+    def __init__(self, raw_data_file_name:str) -> None:
+        self.two_d_array = np.load(raw_data_file_name)
+
+class PokerTest(Test):
+    """Takes 2darray as an argument
+    Does calculations for the poker test.
+    Occurencies of poker hands drawn from either the first 5 cards or proper poker way(2hand cards and 3 flop cards)
+    Saves the image of the plotted result. where x=pokerhand type name and y=Occurencies
+    """
+    def __init__(self) -> None:
+        super().__init__()
+
+    def run(self):
+        pass
+
+class PatternMatching(Test):
+    """Does pattern matching shaningans
+    """
+    def __init__(self) -> None:
+        super().__init__()
+
 RUNS = 6000 # how many times will it shuffle
 ALGORITHM = "np_random_shuffle"
 num_shuffles = 1
@@ -52,4 +133,4 @@ card2 =  Card(int(loaded_array[len(loaded_array)-1, 1])) # takes the lenght aka 
 card3 =  Card(int(loaded_array[0, 2])) # Takes 3 card from the first row, np.array syntax
 card4 =  Card(int(loaded_array[0][3])) # Less efficient python list syntax of taking the 4 card from first row
 print(card, card2,card3,card4,card5)
-print(loaded_array[0, 0]) # repr as np.int8 (not python int, be cerefull).has it own repr and str method
+print(loaded_array[0, 0]) # r pfepr as np.int8 (not python int, be cerefull).has it own repr and str method
--- test.py
+++ test.py
@@ -1,25 +1,7 @@
-from poker_calc.deck.deck import Deck as Dk
-from poker_calc.utils.ranker import evaluate_hand
-from poker_calc.constants.combos import COMBOS 
-from copy import deepcopy
-
-class Deck(Dk):
-    def __init__(self) -> None:
-        super().__init__()
-       
-    # def riffle_shuffel(self):
-    #     pass
-         
-
-
-deck_orginal = Deck()
-
-for i in range(2):
-    deck = deepcopy(deck_orginal) # Use deep copy to have the same original object rather than creating a new deck
-    print(deck)
-    # print(deck.show_all_cards())
-    deck.riffle_shuffel()
-    deck.show_all_cards()
-    res =  evaluate_hand(deck.cards[:5])
-    print(COMBOS.get(res))
-    
+from main import Simulation, Test
+# from main import Deck
+sim = Simulation()
+sim.run('np_shuffle')
+sim.save()
+test1 = Test('np_shuffle-1.npy')
+print(test1.two_d_array)
