4d4af15c57c6056b7ca40943b42b471c347a8e7c
abishevs <abisevs.eduards@gmail.com>
2023-10-01 16:53:09 +0200

    fix: more effecient way of drawing 5 cards for poker test

--- main.py
+++ main.py
@@ -56,9 +56,9 @@ class Simulation:
 
 
     """
-    def __init__(self) -> None:
-        self.num_runs : int = 10
-        self.num_shuffles : int = 1 
+    def __init__(self, num_runs = 1000000, num_shuffles = 1) -> None:
+        self.num_runs : int = num_runs
+        self.num_shuffles : int = num_shuffles 
         self.shuffle_name = ""
         self.raw_data = np.zeros((self.num_runs, 52), dtype='int8')  # int8 can store -+127, Do upcasting if numbers could exceed
 
@@ -86,24 +86,33 @@ class Test:
     """An base class of tests, mby if it has merit
     """
     def __init__(self, raw_data_file_name:str, folder_name = "Grahs&stuff") -> None:
-        file_name = raw_data_file_name.removesuffix(".npy").split('-')
         self.raw_data_file_name = raw_data_file_name.removesuffix(".npy")
-        self.shuffle_name = file_name[0].capitalize().replace("_", " ") 
-        self.shuffle_runs = file_name[1]
-        self.folder_path = self.folder_name(folder_name)
-        self.result_file_name = f"{self.folder_path}/{self.raw_data_file_name}"
+        self.shuffle_name = sanitise_name(raw_data_file_name)
+        self.shuffle_runs =  sanitise_name(raw_data_file_name, -1)
+        self.result_file_name = f"{get_path(folder_name)}/{self.raw_data_file_name}"
         self._shuffled_decks = np.load(raw_data_file_name)
 
     @property
     def shuffled_decks(self):
         return self._shuffled_decks
     
-    def folder_name(self,folder_name):
+def get_path(folder_name):
         current_dir = os.getcwd()
         path = os.path.join(current_dir, folder_name)
         if not os.path.exists(path):
             os.mkdir(path)
         return path
+
+def sanitise_name(file_name:str, index:int = 0):
+        """Splits string from shuffle_name-runs.npy 
+        to shuffle name human readable and runs as char 
+        """
+        file_name_split = file_name.removesuffix(".npy").split('-')
+        if index == 0:
+            return file_name_split[index].capitalize().replace("_", " ")
+        return file_name_split[-1]
+
+
 
 
      
@@ -116,13 +125,12 @@ class PokerTest(Test):
     def __init__(self, shuffled_decks_file) -> None:
         super().__init__(shuffled_decks_file)
     
-    def draw_hand(self,column):
-        """Returns an np.array with size 5"""
-        return self.shuffled_decks[column, :5]
-
     def run(self):
-        for i in range(len(self.shuffled_decks)):
-            print(self.draw_hand(i))
+        five_card_decks = self.shuffled_decks[:,[0,2,5,6,7]] # two player poker game, p1 two cards + flop
+        hand_types = np.apply_along_axis(evaluate_hand, axis=1, arr=five_card_decks) # returns 1d array containing hand_types
+        occurencies = np.unique(hand_types)
+        theoretical_occurencies = None # calculate theoritcal occurencies of each hand type to draw an red line idicating where idealy it should have landed.
+        print(hand_types)
 
 class StdMean(Test):
     """Does pattern matching shaningans
@@ -142,48 +150,47 @@ class StdMean(Test):
         plt.savefig(self.result_file_name, facecolor='y', bbox_inches="tight",
                     pad_inches=0.3, transparent=True)
 
-def evaluate_hand(hand: list[Card]) -> int:
-    """ Assuming that hand list is sorted by the highest rank as the last elemenet
-    returns relative hand strenght value from 0-9
+def evaluate_hand(hand: np.ndarray) -> np.int8:
+    """ Cards come in as int8 0-51. suite and rank are extracted.
+    card // 13 = suite and card % 13 = rank
     """
-    MAX = 5
-    if len(hand) == MAX:
-        # High card - low card == 4 # if True its a straight
-        ranks = [card.get_rank for card in hand]
-        dict_ranks = Counter(ranks)
-        common_ranks = sorted(list(dict_ranks.values()))
+    assert hand.shape == (5,)
+    assert hand.dtype == 'int8'
+        
+    ranks = hand % 13
+    ranks.sort()
+    suites = hand // 13 
 
-        one_pair = common_ranks[-1] == 2 and common_ranks[-2] == 1
-        two_pair = common_ranks[-1] == 2 and common_ranks[-2] == 2
-        trips =  max(common_ranks) == 3 and common_ranks[-2] == 1 # aka theere of a kind
-        straight = hand[-1].value - hand[0].value == 4 
-        wheel = hand[-1].value == 14 and hand[-2].value == 5 # when Ace is the low card straight to 5
-        flush = len(set(card.suite for card in hand)) == 1 
-        fullhouse =  max(common_ranks) == 3 and common_ranks[-2] == 2
-        quads =  max(common_ranks) == 4 # aka four of a kind
-        royal_flush =  straight and flush and hand[0].value == 10
 
-        if one_pair: 
-            return 1 
-        elif two_pair: 
-            return 2
-        elif trips: 
-            return 3
-        elif not flush and straight or wheel:
-            return 4
-        elif not straight and flush:
-            return 5
-        elif fullhouse:
-            return 6
-        elif quads:
-            return 7
-        elif wheel or straight and flush: # Straight flush 
-            return 8 
-        elif royal_flush:
-            return 9
-        return 0
-    else:
-        raise ValueError("Gotta be excatly 5 cards")
+    one_pair = common_ranks[-1] == 2 and common_ranks[-2] == 1
+    two_pair = common_ranks[-1] == 2 and common_ranks[-2] == 2
+    trips =  max(common_ranks) == 3 and common_ranks[-2] == 1 # aka theere of a kind
+    straight = hand[-1].value - hand[0].value == 4 
+    wheel = hand[-1].value == 14 and hand[-2].value == 5 # when Ace is the low card straight to 5
+    flush = len(set(card.suite for card in hand)) == 1 
+    fullhouse =  max(common_ranks) == 3 and common_ranks[-2] == 2
+    quads =  max(common_ranks) == 4 # aka four of a kind
+    royal_flush =  straight and flush and hand[0].value == 10
+
+    if one_pair: 
+        return 1 
+    elif two_pair: 
+        return 2
+    elif trips: 
+        return 3
+    elif not flush and straight or wheel:
+        return 4
+    elif not straight and flush:
+        return 5
+    elif fullhouse:
+        return 6
+    elif quads:
+        return 7
+    elif wheel or straight and flush: # Straight flush 
+        return 8 
+    elif royal_flush:
+        return 9
+    return 0
 
 # RUNS = 6000 # how many times will it shuffle
 ALGORITHM = "np_random_shuffle"
--- test.py
+++ test.py
@@ -1,6 +1,45 @@
-from main import Simulation, Test
-# from main import Deck
-sim = Simulation()
-sim.run('np_shuffle')
-sim.save()
-test1 = Test('np_shuffle-1.npy')
+# from main import Simulation, Test
+# from poker_calc.constants.combos import COMBOS
+# import matplotlib.pyplot as plt
+# import numpy as np
+#
+# # Your frequency data (just for demonstration)
+# freq_data = np.random.randint(0, 100, 10)
+#
+# # Create the plot
+# plt.bar(range(10), freq_data)
+#
+# # Set x-tick labels
+# plt.xticks(range(10), [COMBOS[i] for i in range(10)], rotation=-90)
+#
+# # Add labels and title
+# plt.xlabel('Hand Type')
+# plt.ylabel('Frequency')
+# plt.title('Poker Hand Frequencies')
+#
+# # Show the plot
+# plt.show()
+#
+import matplotlib.pyplot as plt
+import numpy as np
+
+# Your actual data. Let's say frequencies for now
+actual_data = [1000, 2000, 3000, 4000, 5000, 4000, 3000, 2000, 1000, 500]
+
+# Your theoretical probabilities. This is just an example.
+theoretical_probabilities = [0.1, 0.15, 0.2, 0.25, 0.05, 0.1, 0.05, 0.05, 0.025, 0.025]
+
+# Calculate expected frequencies
+total_rounds = sum(actual_data)
+expected_frequencies = [prob * total_rounds for prob in theoretical_probabilities]
+
+# x-axis labels
+label_dict = {0: "High Card", 1: "Pair", 2: "Two Pair", 3: "Three of a Kind", 4: "Straight", 5: "Flush", 6: "Full House", 7: "Four of a Kind", 8: "Straight Flush", 9: "Royal Flush"}
+
+# Plotting
+plt.bar(range(10), actual_data, alpha=0.6, label='Actual Data')
+plt.plot(range(10), expected_frequencies, color='red', marker='o', linestyle='dashed', linewidth=2, markersize=6, label='Theoretical Frequencies')
+plt.xticks(range(10), [label_dict[i] for i in range(10)], rotation=90)
+plt.legend()
+plt.show()
+
