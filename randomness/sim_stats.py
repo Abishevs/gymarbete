473a42b0778049ef6ff2adb7c1538bb384b8ab65
abishevs <abisevs.eduards@gmail.com>
2023-11-12 14:39:24 +0100

    fix: small fixes & Pokertest is now more flexible

--- randomness/sim_stats.py
+++ randomness/sim_stats.py
@@ -1,4 +1,6 @@
 from enum import Enum
+import math
+import re
 from typing import Union
 from randomness.utils import (evaluate_hand, 
                    shuffling_algo_wrapper,
@@ -12,16 +14,30 @@ import pandas as pd
 from scipy.stats import chisquare
 import os
 
-DATASET_LENGHT = 3248700
+# DATASET_LENGHT = 3248700
+DATASET_LENGHT = 3250000
 ROW_LENGHT = 52
 DTYPE = np.int8
 
 class FileLoadingError(Exception):
     pass
 
+class DatasetShapeError(Exception):
+    pass
+
+class InvalidFrequencyError(Exception):
+    pass
+
+class InvalidFileNameFormatError(Exception):
+    pass
+
 class Column(Enum):
+    """
+    columns in Pandas data frame for the tests
+    """
+
     ALGORITHM =  "Algoritm"
-    ITERATIONS =  "Iteration"
+    ITERATIONS =  "Iterationer"
     CHI2_STAT = "X^2"
     P_VALUE = "PVärde"
     CRITICAL_VALUE = "Gräns värde"
@@ -85,17 +101,17 @@ class BaseTest:
     test implementions like Poker test or StdMean.
 
     Attributes:
-    dataset (numpy.ndarray): loaded dataset for the tests.
-    dataset_file_name (str): dataset file name for extracting name and iterations
-    table (pandas.DataFrame): Table to store test results from all tests 
+        dataset (numpy.ndarray): loaded dataset for the tests.
+        dataset_file_name (str): dataset file name for extracting name and iterations
+        table (pandas.DataFrame): Table to store test results from all tests 
 
     Methods:
-    __init__(row_index:int, folder_name:str= "Result"): pass in row index and optional folder for result graphs
-    create_new_row(cls): class  method to Initialize an new row and get index of that row
-    load_dataset(file_path:str): given path to dataset loads it, should be an 1d array stored in .bin 
-    save_table(file_name:str): Saves the result to a csv file
-    run(): Abstract method, to be implemented by subclasses
-    save(): Abstract method, save grahps/tables, to be implemnted by subclasses for specific tests
+        __init__(row_index:int, folder_name:str= "Result"): pass in row index and optional folder for result graphs
+        create_new_row(cls): class  method to Initialize an new row and get index of that row
+        load_dataset(file_path:str): given path to dataset loads it, should be an 1d array stored in .bin 
+        save_table(file_name:str): Saves the result to a csv file
+        run(): Abstract method, to be implemented by subclasses
+        save(): Abstract method, save grahps/tables, to be implemnted by subclasses for specific tests
     
     """
     dataset = np.array([])
@@ -104,19 +120,12 @@ class BaseTest:
 
     def __init__(self,row_index=None, folder_name = "Result") -> None:
         """
-        Mandatory that dataset is an flatten 1D arrays of row= deck [u8, 52] and len 3248700 rows
-        and file extension .bin (binary format)
-
-        Note:
-            Can be changed by changing constants: DATASET_LENGHT but some test might break!!!
-
-        Params:
+        Arguments:
             row_index(BaseTest.create_new_row()): create a shared row for subclassed tests
             folder_name(str): grahps result folder name
         """
 
-        self.dataset_file_name = BaseTest.dataset_file_name
-        self.file_name = self.dataset_file_name.removesuffix(".bin")
+        self.file_name = BaseTest.dataset_file_name
         self.shuffle_name = get_shuffle_name(self.file_name)
         self.shuffle_runs =  get_shuffle_runs(self.file_name)
         self.result_folder = folder_name
@@ -145,7 +154,7 @@ class BaseTest:
         """
         Add a value to pandas dataframe
 
-        params:
+        Arguments:
             column (Column | str): column in dataframe
             value (str): value to add
 
@@ -164,49 +173,86 @@ class BaseTest:
         return BaseTest.dataset
     
     @classmethod
-    def load_dataset_np(cls, file_name:str):
-        """Loads .npy file. which contains an 2D array
-        
-        Arguments:
-            file_name (string): Path to dataset
+    def validate_file_name(cls, file_name:str):
         """
-        try:
-            cls.dataset =  np.load(file_name)
-            cls.dataset_file_name = file_name
-        except Exception as e:
-            raise FileLoadingError(f"Failed to load file {file_name}") from e
+        Checks if file follows: algo name seperated by underscore
+        followed by a hypheen follow by an one or more digits indicating interations
+
+        Arguments:
+            file_name(str): file name without file extension
+
+        Raises:
+            InvalidFileNameFormatError: if file format is structured wrong 
+        """
+
+        pattern = re.compile(r'^[a-zA-Z0-9_]+-\d+$')
+        if not pattern.match(file_name):
+            raise InvalidFileNameFormatError(f"Invalid file name format: '{file_name}'. Expected format 'algo_name-iterationsnum'")
+
 
     @classmethod
     def load_dataset(cls, file_path:str):
         """
         Loads a dataset given path, should follow an specific file format.
-        and contain an flatten 2D array. 
 
         Example:
-        riffle_shuffle-3.bin 
+            riffle_shuffle-3.bin or riffle_shuffle-3.npy [should be an 2d array]
+
+        Wrong example:
+            riffleshuffle.bin [should include iterations as int]
 
         Arguments:
             file_path (string): Path to dataset
         
         Raises: 
-        FileLoadingError: If it can't find file, or it's in wrong format
+            InvalidFileNameFormatError: if file format is structured wrong 
+            FileLoadingError: If it can't find file, or it's in wrong format
+            DatasetShapeError: if it can't reshape given array
 
         """
+        file_name, file_extension = os.path.splitext(file_path)
+        file_name = os.path.basename(file_name)
 
         try:
-            flatten_dataset = np.fromfile(file_path, dtype=DTYPE)
-            dataset = flatten_dataset.reshape((DATASET_LENGHT, ROW_LENGHT))
-            cls.dataset = dataset 
-            cls.dataset_file_name = os.path.basename(file_path) 
-        except Exception as e:
-            raise FileLoadingError(f"Failed to load file {file_path}") from e
+            # validate that file is an proper format, raise an error otherwise
+            cls.validate_file_name(file_name)
+
+            if file_extension == ".npy":
+                cls.dataset = np.load(file_path)
+                if cls.dataset.ndim != 2 or cls.dataset.shape[1] != ROW_LENGHT:
+                    raise DatasetShapeError(f"Shape mismatch for .npy file: Expected 2D array with {ROW_LENGHT} columns, got {cls.dataset.shape}")
+
+            else:
+                flatten_dataset = np.fromfile(file_path, dtype=DTYPE)
+
+                # check if flatten dataset is an multiple of rows
+                if flatten_dataset.size % ROW_LENGHT != 0:
+                    raise DatasetShapeError(f"Dataset size missmatch: Expected multiple of {ROW_LENGHT}, got {flatten_dataset.size}")
+
+                # calculate number of rows
+                dataset_lenght = flatten_dataset.size // ROW_LENGHT
+
+                # reshape back into 2D array and set it as class global variable
+                cls.dataset = flatten_dataset.reshape((dataset_lenght, ROW_LENGHT))
+        
+            # extract shuffle name + iterations from file name
+            cls.dataset_file_name = os.path.basename(file_name)
+
+            # check if its expected shape
+            if cls.dataset.shape[1] != ROW_LENGHT:
+                raise DatasetShapeError(f"Row lenght missmatch: Expected {ROW_LENGHT}, got {cls.dataset.shape[1]}")
+
+        # if file was not found
+        except OSError as e:
+            raise FileLoadingError(f"File not found {file_path}") from e
+
     
     @classmethod
     def clear_dataset(cls):
         cls.dataset = np.array([])
 
     @classmethod
-    def save_table(cls, result_folder:str= "Result", result_file_name:str="table"):
+    def save_table(cls, result_folder:str = "Result", result_file_name:str="table"):
         result_file_name =f"{result_file_name}.csv" 
         result_file_name = f"{os.path.join(result_folder, result_file_name)}"
         cls.table.to_csv(result_file_name, index=False)
@@ -231,8 +277,37 @@ class PokerTest(BaseTest):
     """
     def __init__(self, row_index) -> None:
         super().__init__(row_index)
+        
     
     def run(self):
+        """
+        Tailord specificly for Pokerhand types as bins for chi-square
+        Thus min dataset lenght:  2598960*1.25 = 3248700 which gives =>
+        5 statisticly expected royal flushes(4*1.25=5). As chi-square test is 
+        invalid if smallest bin is smaller than 5 by it's defintion: 
+        https://www.itl.nist.gov/div898/handbook/eda/section3/eda35f.htm
+
+        Adds result from pokertest to pandas dataframe: Basetest.table
+
+        Raises:
+            InvalidFrequencyError: if expected frequency for royal flushes is less than 5
+
+        """
+        # expected frequencies 52 choose 5 = 2'598'960
+        # thus each handtype is distributed:
+        # min is default * 1.25 to get atleast 5 expected royal flushes
+        default = math.comb(52, 5)
+        if self.shuffled_decks.shape[0] < (default*1.25):
+            raise InvalidFrequencyError(f"Dataset: {self.shuffled_decks.shape[0]} is too small to perform pokertest")
+
+        f_exp = np.array([1302540,1098240,123552 ,54912,10200,5108, 3744, 624, 36, 4], dtype=np.float64)
+
+        # get the factor from the dataset divded by the default for a single deck
+        factor = self.shuffled_decks.shape[0] / default
+
+        # update expected freqencies with the desired factor
+        f_exp *= factor
+
         five_card_decks = self.shuffled_decks[:,[0,2,5,6,7]] # two player poker game, p1 two cards + flop
         result = np.apply_along_axis(evaluate_hand, axis=1, arr=five_card_decks) # returns 1d array containing hand_types
 
@@ -243,9 +318,6 @@ class PokerTest(BaseTest):
 
         # Fill in the observed counts into the init array
         f_obs[hand_types] = observed
-
-        # expected frequencies
-        f_exp = np.array([1628176,1372800,154440,68639,12751,6383,4681,780,45,5])
 
         CRITICAL_VALUE = 16.918977604620448 # with p-value 0.05 and df= 9
         P_VALUE = 0.05
@@ -266,7 +338,9 @@ class StdMean(BaseTest):
     1) Calculates mean position of each card in the deck
     2) Calcualtes how much each card deviates from mean position.
     Expected mean: 26 [52/2] 
-    Expected std: as long as possible :D
+    Expected std: 
+
+    Saves result as image of the plot in result directory
     """
     def __init__(self, row_index) -> None:
         super().__init__(row_index)
